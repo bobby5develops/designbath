a1ba393685c515a47a69550845524cd19694d449
bobby5develops <bobby5develops@gmail.com>
2015-02-09 18:30:15 -0500

    feb-9 hash code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 
-$(document).ready(function(){
+$(document).ready(function(){ //executed function after the page has fully loaded;
+	var checkHash,
+	    lastHash = "";//store current URL hash in empty string;
+
+	function checkHash(hash){//test for current hash parameter, and load content
+		if (!hash) {
+			hash = window.location.hash;//if no parameter is provided, use the hash value from current page
+		}else if (hash != lastHash) {//if hash value has indeed changed, 
+            lastHash = hash;//update current hash
+            loadPage(hash);//load the new page associated with current hash
+		};
+
+	}
+	checkHash();//check if the URL has a reference to a page and load it;
+    
+    function loadPage(){//load pages via ajax 
+
+    }
+
 	/*navigation accordion*/
 	$("#accordion h3").click(function(){
 		//slide up all link sub-menu lists
@@ -10,5 +28,8 @@ $(document).ready(function(){
 		};
 	})
 
+	/*event listener for navigation menu hash change*/
+	$("#accordion ul.list li ul li a")
 
-});
+
+}); 
